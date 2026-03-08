8c8b8c6f0a649c6897bc1aad625859269992b899
bookishdemands <bookishdemands@proton.me>
2026-03-07 21:38:17 -0600

    Update props.js

--- js/character/props.js
+++ js/character/props.js
@@ -1,1 +1,15 @@
-
+export const PROPS = [
+  "holding a phone like she just read something messy",
+  "holding an e-reader",
+  "holding an annotated romance book with tabs",
+  "holding an open book dramatically",
+  "holding a thriller book open",
+  "holding a dark romance book",
+  "holding a luxury hardcover romance novel",
+  "holding a stack of dark romance books",
+  "holding a reading journal + pen",
+  "holding a pink e-reader",
+  "wearing headphones listening to an audiobook",
+  "holding a paranormal romance novel",
+  "holding an open gothic romance book"
+];
