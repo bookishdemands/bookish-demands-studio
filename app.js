bf5c25657d9c08f74bee9c4d8447f5392baadcd3
bookishdemands <bookishdemands@proton.me>
2026-03-07 18:37:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+document.getElementById("generateBtn").onclick = function(){
 
+const mode = document.getElementById("studioMode").value;
+
+if(mode === "character"){
+document.getElementById("output").value =
+"Character Studio generator coming online...";
+}
+
+if(mode === "sticker"){
+document.getElementById("output").value =
+"Sticker Studio generator coming online...";
+}
+
+};
