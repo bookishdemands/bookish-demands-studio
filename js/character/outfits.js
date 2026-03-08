533a5f7d6d6a4a00ad470723d76dbbcd5f6e121f
bookishdemands <bookishdemands@proton.me>
2026-03-07 21:39:04 -0600

    Update outfits.js

--- js/character/outfits.js
+++ js/character/outfits.js
@@ -1,1 +1,19 @@
-
+export const OUTFITS = [
+  "sleek glam casual outfit",
+  "fitted rich girl casual look",
+  "cute but sharp street glam outfit",
+  "soft luxe lounge set",
+  "hyper-feminine glam outfit",
+  "street glam casual outfit",
+  "sharp fitted glam outfit",
+  "dark glam outfit",
+  "dark luxe reader outfit",
+  "quiet luxury glam outfit",
+  "urban luxe fashion styling",
+  "tailored glam outfit",
+  "girly glam outfit",
+  "casual glam fit",
+  "cozy but pretty lounge look",
+  "dark romantic glam outfit",
+  "moody luxe glam outfit"
+];
