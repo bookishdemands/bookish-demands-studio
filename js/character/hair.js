349dcf2304b0e1bbf01aaaf2baad27f90b67ac23
bookishdemands <bookishdemands@proton.me>
2026-03-07 21:39:29 -0600

    Update hair.js

--- js/character/hair.js
+++ js/character/hair.js
@@ -1,1 +1,18 @@
-
+export const HAIR = [
+  "sleek straight hair with laid edges",
+  "long glossy black waves",
+  "sharp middle-part silk press",
+  "long glam curls",
+  "half-up glossy curls",
+  "high curly ponytail",
+  "messy glam bun",
+  "deep side part glam waves",
+  "voluminous dark glam curls",
+  "soft expensive curls",
+  "waist-length curls",
+  "sleek polished silk press",
+  "glossy long waves",
+  "soft loose curls",
+  "dark romantic curls",
+  "sleek dramatic center part"
+];
