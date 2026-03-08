504be452d01b731f2032b6ce30f8b9c7e7430df9
bookishdemands <bookishdemands@proton.me>
2026-03-07 21:40:04 -0600

    Update palettes.js

--- js/character/palettes.js
+++ js/character/palettes.js
@@ -1,1 +1,19 @@
-
+export const PALETTES = [
+  "black + hot pink + silver",
+  "matte black + electric magenta",
+  "charcoal + soft pink glow",
+  "blush pink + cream + gold",
+  "soft mauve + champagne",
+  "hot pink + black + chrome",
+  "electric magenta + violet",
+  "black + red + silver",
+  "gunmetal + ivory + crimson",
+  "oxblood + matte black + antique gold",
+  "blood red + black + steel",
+  "cream + mocha + gold",
+  "champagne + taupe + ivory",
+  "midnight blue + lavender",
+  "deep plum + blush",
+  "midnight blue + violet + black",
+  "black + plum + silver"
+];
