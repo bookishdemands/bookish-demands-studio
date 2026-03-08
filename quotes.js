9b720d94872945b3eb3ff043063687bfb3050949
bookishdemands <bookishdemands@proton.me>
2026-03-07 18:34:55 -0600

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,1 +1,14 @@
+export const MICRO_QUOTES = [
+  "Certified Delulu",
+  "Kindle Confidential",
+  "Chapter 27 Energy",
+  "Main Character Energy",
+  "Plot Twist Pending",
+  "Do Not Disturb",
+  "Booked & Busy",
+  "Soft Life Mode",
+  "Morally Gray Approved",
+  "Unhinged (Respectfully)"
+];
 
+export const QUOTE_BANKS = {};
