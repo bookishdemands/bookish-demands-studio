06581cfb704d07e999ae2f7eae639ce82e30505c
bookishdemands <bookishdemands@proton.me>
2026-03-07 21:36:25 -0600

    Update expressions.js

--- js/character/expressions.js
+++ js/character/expressions.js
@@ -1,1 +1,14 @@
-
+export const EXPRESSIONS = [
+  "dramatic side-eye",
+  "unimpressed deadpan",
+  "smug knowing smirk",
+  "soft knowing smile",
+  "mischievous grin",
+  "wide shocked eyes",
+  "dramatic gasp",
+  "dangerous smirk",
+  "dreamy delulu smile",
+  "confident main character smirk",
+  "haunted intrigued stare",
+  "stunned reader face"
+];
