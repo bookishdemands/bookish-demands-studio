98176f36827602f0acc787f570b26d18761f2522
bookishdemands <bookishdemands@proton.me>
2026-03-08 15:00:25 -0500

    Create products.js

---
+++ js/sticker/products.js
@@ -0,0 +1,19 @@
+export const STICKER_PRODUCTS = [
+  "quote sticker",
+  "reaction sticker",
+  "kindle insert sticker",
+  "warning label sticker",
+  "book club sticker",
+  "dark romance sticker",
+  "reader mood sticker",
+  "annotation sticker",
+
+  "energy drink can sticker",
+  "romance reader juice box sticker",
+  "dark romance IV bag sticker",
+  "book hangover medicine bottle sticker",
+  "romance addiction prescription bottle sticker",
+  "reader survival kit sticker",
+  "book boyfriend warning label sticker",
+  "morally grey lover caution label sticker"
+];
