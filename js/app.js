42edb8c9540f35208ad0cb5fa4a265ab50fcc78c
bookishdemands <bookishdemands@proton.me>
2026-03-07 21:53:16 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,68 +1,93 @@
 import { ARCHETYPES } from "./core/archetypes.js";
+import { HAIR } from "./character/hair.js";
+import { OUTFITS } from "./character/outfits.js";
+import { EXPRESSIONS } from "./character/expressions.js";
+import { POSES } from "./character/poses.js";
+import { PROPS } from "./character/props.js";
+import { PALETTES } from "./character/palettes.js";
+
 import { buildCharacterPrompt } from "./character/promptBuilder.js";
 
-const studioMode = document.getElementById("studioMode");
 const archetypeSelect = document.getElementById("archetypeSelect");
+const hairSelect = document.getElementById("hairSelect");
+const outfitSelect = document.getElementById("outfitSelect");
+const expressionSelect = document.getElementById("expressionSelect");
+const poseSelect = document.getElementById("poseSelect");
+const propSelect = document.getElementById("propSelect");
+const paletteSelect = document.getElementById("paletteSelect");
+
 const randomArchetypeBtn = document.getElementById("randomArchetypeBtn");
+const randomizeAllBtn = document.getElementById("randomizeAllBtn");
 const generateBtn = document.getElementById("generateBtn");
+
 const output = document.getElementById("output");
-const characterControls = document.getElementById("characterControls");
 
-function pick(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+function pick(arr){
+return arr[Math.floor(Math.random()*arr.length)];
 }
 
-function fillArchetypes() {
-  archetypeSelect.innerHTML = "";
+function fillSelect(select, list){
 
-  ARCHETYPES.forEach((archetype) => {
-    const opt = document.createElement("option");
-    opt.value = archetype;
-    opt.textContent = archetype;
-    archetypeSelect.appendChild(opt);
-  });
-}
+select.innerHTML="";
 
-function getSelectedArchetype() {
-  return archetypeSelect.value || ARCHETYPES[0];
-}
-
-function setRandomArchetype() {
-  archetypeSelect.value = pick(ARCHETYPES);
-}
-
-function generateCharacterPrompt() {
-  const archetype = getSelectedArchetype();
-  const prompt = buildCharacterPrompt(archetype);
-  output.value = prompt;
-}
-
-function updateModeUI() {
-  const mode = studioMode.value;
-
-  if (mode === "character") {
-    characterControls.style.display = "block";
-    generateBtn.textContent = "Generate Character Prompt";
-  } else {
-    characterControls.style.display = "none";
-    generateBtn.textContent = "Sticker Studio Coming Next";
-    output.value = "Sticker Studio wiring comes after Character Studio is finalized.";
-  }
-}
-
-fillArchetypes();
-updateModeUI();
-
-studioMode.addEventListener("change", updateModeUI);
-
-randomArchetypeBtn.addEventListener("click", () => {
-  setRandomArchetype();
+list.forEach(item=>{
+const opt=document.createElement("option");
+opt.value=item;
+opt.textContent=item;
+select.appendChild(opt);
 });
 
-generateBtn.addEventListener("click", () => {
-  if (studioMode.value === "character") {
-    generateCharacterPrompt();
-  } else {
-    output.value = "Sticker Studio wiring comes after Character Studio is finalized.";
-  }
-});
+}
+
+function initialize(){
+
+fillSelect(archetypeSelect,ARCHETYPES);
+fillSelect(hairSelect,HAIR);
+fillSelect(outfitSelect,OUTFITS);
+fillSelect(expressionSelect,EXPRESSIONS);
+fillSelect(poseSelect,POSES);
+fillSelect(propSelect,PROPS);
+fillSelect(paletteSelect,PALETTES);
+
+}
+
+function randomizeAll(){
+
+hairSelect.value=pick(HAIR);
+outfitSelect.value=pick(OUTFITS);
+expressionSelect.value=pick(EXPRESSIONS);
+poseSelect.value=pick(POSES);
+propSelect.value=pick(PROPS);
+paletteSelect.value=pick(PALETTES);
+
+}
+
+function generate(){
+
+const archetype=archetypeSelect.value;
+
+const options={
+hair:hairSelect.value,
+outfit:outfitSelect.value,
+expression:expressionSelect.value,
+pose:poseSelect.value,
+prop:propSelect.value,
+palette:paletteSelect.value
+};
+
+const prompt=buildCharacterPrompt(archetype,options);
+
+output.value=prompt;
+
+}
+
+randomArchetypeBtn.onclick=()=>{
+archetypeSelect.value=pick(ARCHETYPES);
+};
+
+randomizeAllBtn.onclick=randomizeAll;
+
+generateBtn.onclick=generate;
+
+initialize();
+randomizeAll();
