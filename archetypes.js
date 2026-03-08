af48d7a544de8f30426dc3be4acd05a5a19377b5
bookishdemands <bookishdemands@proton.me>
2026-03-07 18:31:16 -0600

    Update archetypes.js

--- archetypes.js
+++ archetypes.js
@@ -1,1 +1,40 @@
+export const ARCHETYPES = [
+  "Petty Reader",
+  "Soft Girl Menace",
+  "Controlled Chaos Reader",
+  "Plot Twist Detective",
+  "Dark Romance Delulu",
+  "Possessive MMC Apologist",
+  "Billionaire MMC Enthusiast",
+  "Villain Defender",
+  "Urban Power Reader",
+  "Boss Woman Reader",
+  "Girly Glam Reader",
+  "Audiobook Multitasker",
+  "Late Night Reader",
+  "Book Hangover Survivor",
+  "Paranormal Romance Devotee",
+  "Suspense Addict",
+  "Haunted Reader"
+];
 
+
+export const ARCHETYPE_QUOTE_BANKS = {
+  "Petty Reader": ["general_urban_bookish","mood_quotes","iykyk"],
+  "Soft Girl Menace": ["girly_feminine_energy","soft_life_self_care"],
+  "Controlled Chaos Reader": ["mood_quotes","iykyk"],
+  "Plot Twist Detective": ["thriller"],
+  "Dark Romance Delulu": ["dark_romance","elite_dominance_lane"],
+  "Possessive MMC Apologist": ["dark_romance","elite_dominance_lane","bde_energy"],
+  "Billionaire MMC Enthusiast": ["elite_dominance_lane","bde_energy"],
+  "Villain Defender": ["dark_romance"],
+  "Urban Power Reader": ["general_urban_bookish","urban_power_couple"],
+  "Boss Woman Reader": ["boss_woman_energy"],
+  "Girly Glam Reader": ["girly_feminine_energy"],
+  "Audiobook Multitasker": ["general_urban_bookish"],
+  "Late Night Reader": ["mood_quotes","dark_romance"],
+  "Book Hangover Survivor": ["mood_quotes"],
+  "Paranormal Romance Devotee": ["paranormal"],
+  "Suspense Addict": ["thriller"],
+  "Haunted Reader": ["paranormal","thriller"]
+};
