d6c072a364cbdbb9e9f547b93d028af2d43df448
bookishdemands <bookishdemands@proton.me>
2026-03-07 19:04:14 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,8 @@
-import { ARCHETYPES } from "./core/archetypes.js";
-import { buildCharacterPrompt } from "./character/characterPromptBuilder.js";
+alert("app.js is loading");
 
-const modeSelect = document.getElementById("studioMode");
-const generateBtn = document.getElementById("generateBtn");
 const output = document.getElementById("output");
-
-function generateCharacterStudio() {
-  const archetype = ARCHETYPES[0];
-  const prompt = buildCharacterPrompt(archetype);
-  output.value = `CHARACTER STUDIO\n\nArchetype: ${archetype}\n\nPROMPT\n${prompt}`;
-}
-
-function generateStickerStudio() {
-  output.value = "Sticker Studio generator coming next.";
-}
+const generateBtn = document.getElementById("generateBtn");
 
 generateBtn.addEventListener("click", () => {
-  const mode = modeSelect.value;
-
-  if (mode === "character") {
-    generateCharacterStudio();
-  } else if (mode === "sticker") {
-    generateStickerStudio();
-  }
+  output.value = "App is connected.";
 });
