722f90507616cf7d33582f8fef1db776119ef948
bookishdemands <bookishdemands@proton.me>
2026-03-07 21:36:48 -0600

    Update microExpressions.js

--- js/character/microExpressions.js
+++ js/character/microExpressions.js
@@ -1,1 +1,14 @@
-
+export const MICRO_EXPRESSIONS = [
+  "one eyebrow raised",
+  "half-lidded unimpressed gaze",
+  "judgy side glance",
+  "tiny smirk corner",
+  "chin slightly lifted",
+  "soft side glance",
+  "eyes widened slightly",
+  "suspicious narrowed gaze",
+  "half-lidded enchanted gaze",
+  "slow dangerous blink",
+  "soft pout",
+  "emotionally invested gaze"
+];
