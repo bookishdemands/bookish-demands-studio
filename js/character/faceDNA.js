ea0e99a8f2ad5943dc81387151947cfe789245c2
bookishdemands <bookishdemands@proton.me>
2026-03-07 21:36:03 -0600

    Update faceDNA.js

--- js/character/faceDNA.js
+++ js/character/faceDNA.js
@@ -1,1 +1,22 @@
+export const COMPLEXIONS = [
+  "deep espresso brown skin",
+  "rich dark chocolate skin",
+  "warm cocoa brown skin",
+  "golden brown skin",
+  "deep mahogany skin",
+  "radiant caramel brown skin"
+];
 
+export const BODY_TYPES = [
+  "curvy feminine silhouette",
+  "soft hourglass figure",
+  "plus-size curvy body type",
+  "mid-size curvy body type"
+];
+
+export const FACE_SHAPES = [
+  "soft oval face shape",
+  "soft heart-shaped face",
+  "soft round face shape",
+  "softened square face shape"
+];
