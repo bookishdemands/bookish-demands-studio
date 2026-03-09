fd77377d6ae93b7e7ccd83f4d5bf9ed128969064
bookishdemands <bookishdemands@proton.me>
2026-03-09 00:22:28 -0500

    Update nailShapes.js

--- js/character/nailShapes.js
+++ js/character/nailShapes.js
@@ -1,1 +1,10 @@
-
+export const NAIL_SHAPES = [
+  "short almond nails",
+  "long almond nails",
+  "coffin nails",
+  "XL coffin nails",
+  "stiletto nails",
+  "square nails",
+  "builder gel nails (high-end)",
+  "press-on nails (premium look)"
+];
