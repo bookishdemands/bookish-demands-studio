fce462d1887014fb36999de0f7cdd8b70db5aa60
bookishdemands <bookishdemands@proton.me>
2026-03-09 00:18:45 -0500

    Update extras.js

--- js/character/extras.js
+++ js/character/extras.js
@@ -1,1 +1,18 @@
-
+export const EXTRAS = [
+  "extra dramatic lashes",
+  "manga lashes",
+  "flutter lashes",
+  "oversized glasses",
+  "gold rim oversized glasses",
+  "cat-eye glasses",
+  "rimless glasses",
+  "nose ring (subtle)",
+  "septum ring (subtle)",
+  "stacked ear piercings",
+  "beauty mark",
+  "baby hairs laid",
+  "rhinestone hair clips",
+  "hair cuffs",
+  "bold statement earrings",
+  "none"
+];
