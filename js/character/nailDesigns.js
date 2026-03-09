bdfe5eced89e871275368fcc947b0a8d8ca1b0dc
bookishdemands <bookishdemands@proton.me>
2026-03-09 00:23:23 -0500

    Update nailDesigns.js

--- js/character/nailDesigns.js
+++ js/character/nailDesigns.js
@@ -1,1 +1,19 @@
-
+export const NAIL_DESIGNS = [
+  "nude gloss",
+  "pink gloss",
+  "milky white",
+  "classic french tip",
+  "colored french tip",
+  "chrome silver",
+  "chrome gold",
+  "rose chrome",
+  "glitter ombré",
+  "cherry red gloss",
+  "deep wine gloss",
+  "black gloss",
+  "black + chrome accent",
+  "rhinestone accent nails",
+  "aura nails",
+  "cat eye gel nails",
+  "marble nails"
+];
