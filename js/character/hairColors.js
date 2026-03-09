49fb7a55a575ec986bd4c3f9a37098775c7e1989
bookishdemands <bookishdemands@proton.me>
2026-03-09 00:19:31 -0500

    Update hairColors.js

--- js/character/hairColors.js
+++ js/character/hairColors.js
@@ -1,1 +1,14 @@
-
+export const HAIR_COLORS = [
+  "jet black hair",
+  "soft black hair",
+  "espresso brown hair",
+  "chocolate brown hair",
+  "warm auburn hair",
+  "copper brown hair",
+  "honey blonde hair",
+  "caramel blonde hair",
+  "golden blonde highlights",
+  "brown hair with blonde money-piece",
+  "burgundy wine hair",
+  "midnight blue-black hair"
+];
