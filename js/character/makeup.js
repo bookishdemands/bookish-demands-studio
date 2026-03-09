80022f3e88d3bf971622dd46109cfeaf5c8d1dcd
bookishdemands <bookishdemands@proton.me>
2026-03-09 00:20:30 -0500

    Update makeup.js

--- js/character/makeup.js
+++ js/character/makeup.js
@@ -1,1 +1,10 @@
-
+export const MAKEUP = [
+  "soft glam makeup",
+  "editorial glam makeup",
+  "glossy lip glam",
+  "bold wing liner glam",
+  "smokey eye glam",
+  "sparkly Y2K shimmer eyeshadow",
+  "rich girl natural glam",
+  "night-out glam"
+];
