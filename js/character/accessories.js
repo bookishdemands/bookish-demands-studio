18569cf97a0ba4d91588e7b5412aaf028b071771
bookishdemands <bookishdemands@proton.me>
2026-03-09 00:24:13 -0500

    Update accessories.js

--- js/character/accessories.js
+++ js/character/accessories.js
@@ -1,1 +1,10 @@
-
+export const ACCESSORIES = [
+  "oversized gold hoop earrings + layered gold necklaces",
+  "gold hoops + chunky chain necklace",
+  "sparkly hair cuffs + hoops",
+  "dainty gold jewelry stack",
+  "bold statement earrings + glossy lip highlight",
+  "choker necklace + hoops",
+  "stacked bracelets + rings",
+  "nose ring + hoops (subtle)"
+];
