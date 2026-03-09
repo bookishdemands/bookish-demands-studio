a432cab87be7fb7e15c911f3c32c84b3b93b26bd
bookishdemands <bookishdemands@proton.me>
2026-03-08 22:56:58 -0500

    Create kindleMicroQuotes.js

---
+++ js/sticker/kindleMicroQuotes.js
@@ -0,0 +1,12 @@
+export const KINDLE_MICRO_QUOTES = [
+  "Morally Gray Approved",
+  "Dark Romance Energy",
+  "Chapter 27 Damage",
+  "Emotionally Booked",
+  "Pretty & Dangerous",
+  "Soft but Delusional",
+  "He Don’t Play About Her",
+  "Main Character Energy",
+  "Unhinged Respectfully",
+  "Fictional Men Only"
+];
