eb339f421a205dbcbeb4e20ef13da2debe9e20ed
bookishdemands <bookishdemands@proton.me>
2026-03-09 21:16:12 -0500

    Create tropeBank.js

---
+++ js/banks/tropeBank.js
@@ -0,0 +1,19 @@
+export const TROPE_BANK = {
+  classic_romance: [],
+  dark_romance: [],
+  black_booktok_urban_romance: [],
+  black_romance: [],
+  hood_billionaire_romance: [],
+  street_loyalty_mmc: [],
+  soft_life_dangerous_man: [],
+  black_dark_romance_obsession: [],
+  urban_power_couple: [],
+  loving_unhinged_black_mmc: [],
+  quiet_menace_mmc: [],
+  provider_alpha_mmc: [],
+  shes_the_only_one_he_listens_to: [],
+  he_doesnt_like_anyone_but_her: [],
+  touch_her_and_die_black_romance: [],
+  street_king_soft_for_her: [],
+  obsessed_rich_man_hood_girl: []
+};
