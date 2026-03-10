08b9c5ad98e11db332514559abcfb572876fd1de
bookishdemands <bookishdemands@proton.me>
2026-03-09 21:19:12 -0500

    Create chapterTitleBank.js

---
+++ js/banks/chapterTitleBank.js
@@ -0,0 +1,8 @@
+export const CHAPTER_TITLE_BANK = {
+  dangerous_mmc: [],
+  street_king_energy: [],
+  expensive_dangerous_men: [],
+  black_romance_devotion: [],
+  reader_mood: [],
+  black_booktok_energy: []
+};
