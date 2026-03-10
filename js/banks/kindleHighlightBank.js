f27838c527731d8a250fffcec432b1c9424794ab
bookishdemands <bookishdemands@proton.me>
2026-03-09 21:15:33 -0500

    Create kindleHighlightBank.js

---
+++ js/banks/kindleHighlightBank.js
@@ -0,0 +1,17 @@
+export const KINDLE_HIGHLIGHT_BANK = {
+  dark_romance: [],
+  dangerous_mmc: [],
+  quiet_power: [],
+  strategy_mmc: [],
+  down_bad_mmc: [],
+  public_claim_mmc: [],
+  wife_energy_mmc: [],
+  provider_protector: [],
+  expensive_dangerous_mmc: [],
+  calm_authority_mmc: [],
+  soft_voice_serious_mmc: [],
+  black_romance: [],
+  urban_romance: [],
+  reader_mood: [],
+  female_pov: []
+};
